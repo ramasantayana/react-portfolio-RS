2ac9c2f37fa4e5b9a38ab0568db9ec819ad9f519
ramasantayana <rama.santayana@uconn.edu>
2023-06-26 06:45:30 -0400

    Added here link for Project 2

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -23,7 +23,7 @@ export default function Portfolio() {
       <div class = "project"> 
         <h2>Project 2</h2>
         <a target="_blank" href="https://ramasantayana.github.io/Work-Day-Scheduler-RS/"> <img height = "500px" src={process.env.PUBLIC_URL + project2Picture} className=" float-left myPic" alt="Rama Santyana Head Shot" /></a>
-      <p>For the code click <a target="_blank" href="https://github.com/ramasantayana/Javascript-Password-Generator-By-RS">here</a></p>
+      <p>For the code click <a target="_blank" href="https://github.com/ramasantayana/Work-Day-Scheduler-RS">here</a></p>
       </div>
 
       <img height = "500px" src={process.env.PUBLIC_URL + project2Picture} className=" float-left myPic" alt="Rama Santyana Head Shot" />
