cb29ed89c7ef174acaf73c55f48058da6bb3e061
ramasantayana <rama.santayana@uconn.edu>
2023-07-06 07:01:16 -0400

    Completed technical elements of portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
 
-const App = () => <PortfolioContainer />;
+const App = () => {
+    return(
+    <div> 
+<PortfolioContainer /> 
+<Footer /> 
+</div>
+    )
+} 
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import  './footer.css';
+let linkedInIcon = "/images/linkedinIcon.png";
+let gitHubIcon = "/images/gitHub.png";
+let instaIcon = "/images/insta.png";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h4>Contact Me</h4>
+      <p>
+        
+      
+        Email: rama.santayana@uconn.edu
+      </p>
+      <a href="https://www.linkedin.com/in/ramasantayana1991" target="_blank"> <img className="myImg"  width="50px" height="50px" src={process.env.PUBLIC_URL + linkedInIcon} alt="linkedIn account" /></a>
+      <a href="https://github.com/ramasantayana" target="_blank"> <img className="myImg"  width="50px" height="50px" src={process.env.PUBLIC_URL + gitHubIcon} alt="gitHub account" /></a>
+      <a href="https://www.linkedin.com/in/ramasantayana1991" target="_blank"> <img className="myImg"  width="50px" height="50px" src={process.env.PUBLIC_URL + instaIcon} alt="insta account" /></a>
+    </div>
+  )
+}
